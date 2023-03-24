27cdce18b856b1f93a8a0d4d8a57ac317e59dba0
Jay Girardot <jgirardot14@gmail.com>
2023-03-24 12:18:03 +0100

    :docs: page html

---
+++ js/baseDeDonnéeTab.js
@@ -0,0 +1,50 @@
+console.log('hello world');
+
+
+const baseDeDonnee = [
+    {
+        title: "Creed 3",
+        realisator: "Michael B. Jordan",
+        timeToMinutes : "116 min",
+        watchTime: "50 min",
+        description: "Dominant toujours le monde de la boxe, Adonis Creed prospère dans sa carrière et sa vie de famille. Lorsque Damian, ami d'enfance et ancien prodige de la boxe, refait surface après avoir purgé une peine de prison, il a hâte de prouver qu'il mérite sa chance dans l'enceinte."
+    },
+    {
+        title: "Avatar: La voie de l'eau",
+        realisator: "James Cameron",
+        timeToMinutes : "190 min",
+        watchTime: "120 min",
+        description: "Jake Sully et Neytiri sont devenus parents. L'intrigue se déroule une dizaine d'années après les événements racontés dans le long-métrage originel. Leur vie idyllique, proche de la nature, est menacée lorsque la « Resources Development Administration », dangereuse organisation non-gouvernementale, est de retour sur Pandora. Contraints de quitter leur habitat naturel, Jake et sa famille se rendent sur les récifs, où ils pensent trouver asile. Mais ils tombent sur un clan, les Metkayina, aux mœurs différentes des leurs..."
+    },
+    {
+        title: "Black Panther : Wakanda Forever",
+        realisator: "Ryan Coogler",
+        timeToMinutes : "162 min",
+        watchTime: "162 min",
+        description: "Après la mort du roi T'Challa alias Black Panther, le Wakanda est en deuil et Ramonda a repris le siège royal avec l'aide de sa fille Shuri, des Dora Milaje, Okoye, Ayo et de M'Baku. Cependant, quand Namor, roi de Talocan, déclare la guerre à la nation, les personnages que nous connaissons vont devoir s'allier à de nouvelles personnes, comme Riri Williams mais aussi à d'anciennes connaissances pour vaincre cette menace."
+    },
+    {
+        title: "Scream VI",
+        realisator: "Matt Bettinelli-Olpin, Tyler Gillett",
+        timeToMinutes : "123 min",
+        watchTime: "10 min",
+        description: "D'Après avoir frappé à trois reprises à Woodsboro, après avoir terrorisé le campus de Windsor et les studios d’Hollywood, Ghostface a décidé de sévir dans Big Apple, mais dans une ville aussi grande que New York personne ne vous entendra crier…"
+    },
+    {
+        title: "Ant-Man et la Guêpe : Quantumania",
+        realisator: "Peyton Reed",
+        timeToMinutes : "126 min",
+        watchTime: "0 min",
+        description: "Scott Lang et Hope Van Dyne font équipe avec les parents de Hope et la fille de Scott pour explorer le monde quantique, interagir avec d'étranges nouvelles créatures et se lancer dans une aventure qui les poussera au-delà des limites de ce qu'ils croyaient possible."
+    },
+    {
+        title: "The Whale",
+        realisator: "Darren Aronofsky",
+        timeToMinutes : "117 min",
+        watchTime: "117 min",
+        description: "Charlie, professeur d'anglais reclus souffrant d'obésité, tente de renouer avec sa fille adolescente pour une ultime chance de rédemption."
+    },
+    
+];
+
+console.log(baseDeDonnee);
